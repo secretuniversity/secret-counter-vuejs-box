0c38c0eb7856bc04a3425cac01317ee4c97db8d9
Alex Werner <alewern@pm.me>
2022-10-25 12:19:06 -0500

    task: updates styles and adds highlight.js styles

--- app/tailwind.config.cjs
+++ app/tailwind.config.cjs
@@ -1,8 +1,14 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./src/**/*.{html,js,vue}"],
+  darkMode: "class",
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "box-yellow": "#F8D948",
+        "primary-dark": "#1a1b26",
+      },
+    },
   },
   plugins: [],
 };
